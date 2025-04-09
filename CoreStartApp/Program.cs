1d9e032be087ce3422cb66509e725257ecb6f795
apocatastas <molchanova.a.a@gmail.com>
2025-04-09 11:26:50 +0300

    Different Initial commit - rollback to 5.0 standarts

--- CoreStartApp/Program.cs
+++ CoreStartApp/Program.cs
@@ -1,35 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+namespace CoreStartApp
 {
-    app.UseDeveloperExceptionPage();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+    }
+   
 }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
 
-app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-});
 
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
-
-app.Run();
